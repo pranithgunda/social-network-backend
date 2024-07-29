b66dc516454b747909f7e77538fb6e7dff4e86d3
Pranith Gunda <pranith0527@yahoo.com>
2024-07-29 10:42:42 -0400

    Added route to add reactions to a thought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -30,8 +30,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
     try {
         const thought = await Thought.create(req.body);
-        const user = await User.findOneAndUpdate(
-            { username: req.body.username },
+        const user = await User.findByIdAndUpdate(
+            { _id: req.body.userId },
             { $addToSet: { thoughts: thought._id } },
             { new: true }
         );
@@ -78,5 +78,24 @@ router.delete('/:id', async (req, res) => {
         res.status(500).json(err);
     }
 });
+
+// Add reactions to thought
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $addToSet: { reactions: req.body } },
+            { new: true });
+        if (!thought) {
+            res.status(404).json({ message: 'No thought found with that id' });
+            return;
+        }
+        res.status(200).json(thought);
+    } catch (err) {
+        console.error(err);
+        res.status(200).json(err);
+    }
+})
+
 
 module.exports = router;
