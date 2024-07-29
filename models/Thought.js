13bea1fa05a45e1814d70de6e994657122db7bfc
Pranith Gunda <pranith0527@yahoo.com>
2024-07-29 02:07:59 -0400

    Modularized reactionSchema into separate file and imported into Thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,17 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
-
-// Schema for reactions
-
-const reactionSchema = new Schema({
-    reactionId: {
-        type: Schema.Types.ObjectId,
-        default: () => new mongoose.Types.ObjectId()
-    },
-    reactionBody: { type: String, required: true, maxLength: 280 },
-    username: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+const Reaction = require('./Reaction');
 
 // Schema for thoughts
 
@@ -19,10 +8,7 @@ const thoughtSchema = new Schema({
     thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
     createdAt: { type: Date, default: Date.now },
     username: { type: String, required: true },
-    reactions: [{
-        type: Schema.Types.ObjectId,
-        ref: reactionSchema
-    }]
+    reactions: [Reaction]
 });
 
 // Initialize our thought model
