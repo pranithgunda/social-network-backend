d9b8cb1ebb136d171934bae21c6bc3e018d8130f
Pranith Gunda <pranith0527@yahoo.com>
2024-07-29 02:20:13 -0400

    Added get route for all users and thoughts

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const { Thought, User } = require('../../models');
 
+// Get all thoughts
+router.get('/', async (req, res) => {
+    try {
+        const thoughts = await Thought.find();
+        res.status(200).json(thoughts);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
 // Add a thought
 router.post('/', async (req, res) => {
     try {
@@ -16,8 +27,9 @@ router.post('/', async (req, res) => {
         res.status(200).json({ message: 'Thought created successfully' })
 
     } catch (err) {
+        console.error(err);
         res.status(500).json(err);
     }
-})
+});
 
 module.exports = router;
