4edb5b53d03636647f51b57354b2d2a17802546c
Pranith Gunda <pranith0527@yahoo.com>
2024-07-29 00:00:40 -0400

    Created thoughtSchema

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,33 @@
-const {Schema, model} = require('mongoose');
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+
+// Schema for reactions
+
+const reactionSchema = new Schema({
+    reactionId:{
+        type:Schema.Types.ObjectId,
+        default:()=> new mongoose.Types.ObjectId()
+    },
+    reactionBody:{type:String, required:true, maxLength:280},
+    username:{type:String, required:true},
+    createdAt:{type:Date, default: Date.now}
+});
 
 // Schema for thoughts
 
-const reactionSchema = new Schema({
-    reactionId:{type:Schema.Types.ObjectId},
-    reactionBody:{type:String,required:true,maxLength:280},
+const thoughtSchema = new Schema({
+    thoughtText:{type:String, required:true, minLength:1, maxLength:280},
+    createdAt:{type:Date,default:Date.now},
     username:{type:String,required:true},
-    createdAt:{type:Date,default: Date.now}
+    reactions:[{
+        type:Schema.Types.ObjectId,
+        ref:reactionSchema
+    }]
 });
+
+// Initialize our thought model
+const Thought =  model('thought',thoughtSchema);
+
+module.exports = Thought;
 
 
