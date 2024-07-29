09ae624d6756362f54d40dc38f6f78fa70c4559a
Pranith Gunda <pranith0527@yahoo.com>
2024-07-29 00:31:21 -0400

    Defined User model with attributes

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const { validate } = require('./Thought');
+const {Schema,model}=mongoose;
+
+// Schema for users
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        unique:true,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        validate:{
+            validator:function(value){
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+            },
+            message:'Invalid email address format'
+        }
+    },
+    thoughts:[{type:Schema.Types.ObjectId,ref:'thought'}],
+    friends:[{type:Schema.Types.ObjectId,ref:'user'}]
+});
+
+// Initialize user model
+const User = model('user',userSchema);
+
+module.exports=User;
